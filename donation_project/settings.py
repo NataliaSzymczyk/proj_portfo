0c66a5586e9e15a70150e55fdb9527b278879517
NataliaSzymczyk <nataliaszymczyk0@gmail.com>
2020-07-07 18:34:23 +0200

    commit

--- donation_project/settings.py
+++ donation_project/settings.py
@@ -123,4 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static') # nie działało
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
--- donation_project/urls.py
+++ donation_project/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from charity_donation.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LandingPage.as_view(), name='index'),
+    path('add-donation/', AddDonation.as_view(), name='add-donation'),
+    path('login/', Login.as_view(), name='login'),
+    path('register/', Register.as_view(), name='register'),
 ]
