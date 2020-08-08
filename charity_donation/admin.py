bbcdf8d48dd97f3eb964d13f91212030cefecd65
NataliaSzymczyk <nataliaszymczyk0@gmail.com>
2020-08-08 14:07:18 +0200

    admin

--- charity_donation/admin.py
+++ charity_donation/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from charity_donation.models import Category, Institution, Donation
+from django.contrib.auth.models import User
+from django.db.models import ProtectedError
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 
 
 admin.site.site_header = "Panel administracyjny"
@@ -13,6 +17,7 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(Institution)
 class InstitutionAdmin(admin.ModelAdmin):
     list_display = ("name", "description", "type", "additional_list")
+    ordering = ['type']
     def additional_list(self, obj):
         return ", ".join([str(o.name) for o in obj.categories.all()])
 
@@ -24,4 +29,11 @@ class DonationAdmin(admin.ModelAdmin):
     raw_id_fields = ('institution',)
     def additional_list2(self, obj):
         return ", ".join([str(o.name) for o in obj.categories.all()])
+
+
+@receiver(pre_delete, sender=User)
+def function(sender, instance, **kwargs):
+    if User.objects.all().count() == 1:
+        raise ProtectedError('zabraniam kasowania użytkownika.', instance)
+
 
